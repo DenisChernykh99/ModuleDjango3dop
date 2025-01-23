366670bf4ce5dda2695e6c1ef53d2d321b26c189
Денис <hegetoer99@gmail.com>
2025-01-23 10:32:24 +0300

    Созданы админ классы и superuser.

--- dopmodule/task1/admin.py
+++ dopmodule/task1/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
 # Register your models here.
+
+# Админка для модели Game
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size',) # Поля для отображения в списке
+    search_fields = ('title',) # Поля для поиска
+    list_filter = ('size', 'cost',) # Фильтрация по размеру и цене
+    list_per_page = 20 # Кол-во записей на странице
+
+
+# Админка для модели Buyer
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'balance', 'age',)
+    search_fields = ('username',)
+    list_filter = ('balance', 'age',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
--- dopmodule/task1/urls.py
+++ dopmodule/task1/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from .views import main, magazine, cart, sign_up_by_html, sign_up_by_django
 
-
+app_name = 'myapp_admin'
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin1/', admin.site.urls),
     path('', main),
     path('catalog/', magazine),
     path('cart/', cart),
